289f8991cc10ab73935c7afdc37b049675f83b89
Zain Azimullah <zain.zain51@gmail.com>
2026-04-08 01:29:23 +1200

    Mixpanel identify

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ This project was built as a **Minimum Viable Product (MVP)** to validate a behav
 ---
 
 ## Live App
-https://wardrobe-manager-tau.vercel.app/
+TBC
 
 ---
 
--- src/utils/analytics.js
+++ src/utils/analytics.js
@@ -2,8 +2,19 @@ import mixpanel from 'mixpanel-browser'
 
 const TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN
 
+function getOrCreateUserId() {
+  const key = '__mp_uid'
+  let id = localStorage.getItem(key)
+  if (!id) {
+    id = crypto.randomUUID()
+    localStorage.setItem(key, id)
+  }
+  return id
+}
+
 if (TOKEN) {
   mixpanel.init(TOKEN, { track_pageview: false, persistence: 'localStorage' })
+  mixpanel.identify(getOrCreateUserId())
 }
 
 export function track(event, props = {}) {
